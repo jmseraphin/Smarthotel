352fe330865b8561253066cc36beb0658781e870
jmseraphin <jmseraphinravelotsara@gmail.com>
2026-05-06 09:21:44 +0300

    add test converge

--- index.js
+++ index.js
@@ -72,6 +72,68 @@
 
 
 
+// const express = require("express");
+// const app = express();
+
+// app.use(express.json());
+
+// // fonctions séparées
+// function applySeason(total, season) {
+//   if (season === "Haute") return total * 1.5;
+//   return total;
+// }
+
+// function applyWeekend(total, hasWeekend) {
+//   if (hasWeekend) return total * 1.2;
+//   return total;
+// }
+
+// function applyLongStay(total, nights) {
+//   if (nights > 7) return total * 0.85;
+//   return total;
+// }
+
+// function applySeaView(total, seaView, nights) {
+//   if (seaView) return total + 30 * nights;
+//   return total;
+// }
+
+// function applyBreakfast(total, clientType, persons, nights) {
+//   if (clientType !== "VIP") return total + 15 * persons * nights;
+//   return total;
+// }
+
+// app.post("/api/book-room", (req, res) => {
+//   let {
+//     pricePerNight,
+//     nights,
+//     season,
+//     hasWeekend,
+//     seaView,
+//     clientType,
+//     persons
+//   } = req.body;
+
+//   let total = pricePerNight * nights;
+
+//   total = applySeason(total, season);
+//   total = applyWeekend(total, hasWeekend);
+//   total = applyLongStay(total, nights);
+//   total = applySeaView(total, seaView, nights);
+//   total = applyBreakfast(total, clientType, persons, nights);
+
+//   res.json({ total });
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+
+
+
+
+
+
 const express = require("express");
 const app = express();
 
@@ -125,6 +187,12 @@ app.post("/api/book-room", (req, res) => {
   res.json({ total });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+// 👉 IMPORTANT ho an'ny test
+if (require.main === module) {
+  app.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+}
+
+// 👉 export
+module.exports = app;
